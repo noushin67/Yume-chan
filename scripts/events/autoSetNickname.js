96ade82ddc313e652abb4a4c300d8fd512ae4b7e
watakhisajib-arch <watakhisajib@gmail.com>
2025-11-16 10:41:03 +0600

    Update autoSetNickname.js

--- scripts/events/autoSetNickname.js
+++ scripts/events/autoSetNickname.js
@@ -12,11 +12,29 @@ module.exports = {
     const { threadID } = event;
     const dataAddedParticipants = event.logMessageData.addedParticipants;
     const botID = api.getCurrentUserID();
-/*
+
     // If the bot was added, set nickname
     if (dataAddedParticipants.some(item => item.userFbId == botID)) {
       api.changeNickname("😾 angry sizukua🥺🌷", threadID, botID);
     }
+
+    // Send welcome message for others
+    for (const participant of dataAddedParticipants) {
+      if (participant.userFbId !== botID) {
+        api.sendMessage(
+          `🌸 Welcome @${participant.fullName}!\nআমাদের গ্রুপে তোমাকে স্বাগতম 🫶✨`,
+          threadID,
+          (err, info) => {
+            api.sendMessage(
+              { mentions: [{ id: participant.userFbId, tag: participant.fullName }] },
+              threadID,
+              null,
+              info.messageID
+            );
+          }
+        );
+      }
+    }
+
   }
 };
-*/
